0e653ef9c80f268ebe1eb9cf257234f70926e10b
Maks <maksim.odinbox@gmail.com>
2021-09-13 11:24:23 +0300

    added homework4

---
+++ homework4/app.js
@@ -0,0 +1,17 @@
+const num1 = +prompt('Enter the first number pleace', ' ');
+const num2 = +prompt('Enter the second number please', ' ');
+const operation = prompt('Enter the operation please', ' ');
+
+
+if (operation === '+') {
+    console.log(num1 + num2);
+} else if (operation === '-') {
+    console.log(num1 - num2);
+} else if (operation === '/') {
+    console.log(num1 / num2);
+} else if (operation === '*') {
+    console.log(num1 * num2);
+} else ((operation === ' '), (num1, num2 === ' ')); {
+    alert('Operation error!');
+} 
+
