58d3fb4bdbc1ad5ed8824d9f309b4fc597a67d84
Maks <maksim.odinbox@gmail.com>
2021-09-22 17:21:12 +0300

    appjs

--- homework7/app.js
+++ homework7/app.js
@@ -1,17 +1,16 @@
-let a = 1
-let b = 1
-let c
-const serialFibNumber = prompt('Какой порядковый номер последовательности фибоначчи вывести?')
+let a = 1;
+let b = 1;
+let c;
+const serialFibNumber = +prompt('Какой порядковый номер ряда фибоначчи вывести?');
 
 for (let d = 3; d <= serialFibNumber; d++) {
-    с = a + b;
-    a = b;
-    b = с;
-    alert(b)
-} 
+  c = a + b;
+  a = b;
+  b = c;
+}
 
-if (serialFibNumber === '1') {
-    alert('1')
-} else if (serialFibNumber === '2') {
-    alert('1')
+if (serialFibNumber === 1 || serialFibNumber === 2) {
+  alert('1');
+} else {
+  alert(c);
 }
