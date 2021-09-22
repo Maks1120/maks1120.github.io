a990b5c0a0463d0a5d694f1cb187cd8b2fcab9ce
Maks <maksim.odinbox@gmail.com>
2021-09-22 16:33:05 +0300

    added_homework7

---
+++ homework7/app.js
@@ -0,0 +1,17 @@
+let a = 1
+let b = 1
+let c
+const serialFibNumber = prompt('Какой порядковый номер последовательности фибоначчи вывести?')
+
+for (let d = 3; d <= serialFibNumber; d++) {
+    с = a + b;
+    a = b;
+    b = с;
+    console.log(b)
+} 
+
+if (serialFibNumber === '1') {
+    сonsole.log('1')
+} else if ('2') {
+    console.log('1')
+}
