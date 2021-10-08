a134aa7f8adcbccf809531e271559b9a2c46c297
Maks <maksim.odinbox@gmail.com>
2021-10-08 21:00:04 +0300

    modified_appjs

--- homework14/app.js
+++ homework14/app.js
@@ -1,12 +1,21 @@
 // 1. Напишите функцию, которая принимает массив с числами, возвращает их сумму. Решите задачу двумя способами: с помощью цикла и с помощью метода reduce
 
-// Method CYCLE
-
+// Method CYCLE 1.1
+// const arr = [2,2];
+//     function arraySum(array) {
+//         let sum = 0;
+//         for(let i = 0; i < array.length; i++) {
+//             sum += array[i];
+//         };
+//     console.log(sum);
+//     };
+// arraySum(arr);
+// Method CYCLE 1.2
 const arr = [2,2];
-    function arraySum(array) {
+    function arraySum(arr) {
         let sum = 0;
-        for(let i = 0; i < array.length; i++) {
-            sum += array[i];
+        for(let array of arr) {
+            sum += array;
         };
     console.log(sum);
     };
@@ -24,7 +33,7 @@ arraySum(arr);
 // 2. Напишите функцию mapToUpperCase(arr), которая принимает массив строк, а возвращает новый массив строк в верхнем регистре. Решите задачу с помощью цикла и метода map
 
 
-// Method CYCLE
+// Method CYCLE 1.1.
 let toUpperCase = ['Soccer', 'Tennis', 'Volleyball'];
     function lettersToUpperCase(toUpperCase) {
         let upp = [];
@@ -35,7 +44,16 @@ let toUpperCase = ['Soccer', 'Tennis', 'Volleyball'];
     }
 lettersToUpperCase(toUpperCase);
 
-
+// Method CYCLE 1.2. (не пойму почему не возвращает массив строк)
+// let toUpperCase = ['Soccer', 'Tennis', 'Volleyball'];
+//     function lettersToUpperCase(toUpperCase) {
+//         let upp = [];
+//         for (let upper of toUpperCase) {
+//             upp = upper.toUpperCase();
+//             console.log(upp);
+//         }
+//     }
+// lettersToUpperCase(toUpperCase);
 
 
 // Method MAP
