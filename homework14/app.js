0014c3ae0e59d31456b03cbda4a2f6686213d231
Maks <maksim.odinbox@gmail.com>
2021-10-27 10:38:48 +0300

    modified app.js

--- homework14/app.js
+++ homework14/app.js
@@ -1,32 +1,25 @@
 // 1. Напишите функцию, которая принимает массив с числами, возвращает их сумму. Решите задачу двумя способами: с помощью цикла и с помощью метода reduce
 
-// Method CYCLE 1.1
-// const arr = [2,2];
-//     function arraySum(array) {
-//         let sum = 0;
-//         for(let i = 0; i < array.length; i++) {
-//             sum += array[i];
-//         };
-//     console.log(sum);
-//     };
-// arraySum(arr);
-// Method CYCLE 1.2
+
+// Method CYCLE
 const arr = [2,2];
     function arraySum(arr) {
         let sum = 0;
         for(let array of arr) {
             sum += array;
         };
-    console.log(sum);
+        return sum;
     };
-arraySum(arr);
+console.log(arraySum(arr));
 
 
 // Method REDUCE
-    const arrSum = [2,4];
+const arrSum = [2,4];
+function sumReduce(arrSum) {
     const sum = arrSum.reduce((result, elem) => result + elem, 0);
-    console.log(sum);
-
+    return sum;
+}
+console.log(sumReduce(arrSum));
 
 
 
@@ -40,29 +33,23 @@ let toUpperCase = ['Soccer', 'Tennis', 'Volleyball'];
         for(let i = 0; i < toUpperCase.length; i++) {
             upp.push(toUpperCase[i].toUpperCase());
         }
-    console.log(upp);
+        return upp;
     }
-lettersToUpperCase(toUpperCase);
+console.log(lettersToUpperCase(toUpperCase));
 
-// Method CYCLE 1.2. (не пойму почему не возвращает массив строк)
-// let toUpperCase = ['Soccer', 'Tennis', 'Volleyball'];
-//     function lettersToUpperCase(toUpperCase) {
-//         let upp = [];
-//         for (let upper of toUpperCase) {
-//             upp = upper.toUpperCase();
-//             console.log(upp);
-//         }
-//     }
-// lettersToUpperCase(toUpperCase);
 
 
 // Method MAP
-let foods = ['Bread', 'Milk', 'Tomatos']
 
-const mapToUpperCase = foods.map( arr => {
-    return arr.toUpperCase()
-});
-console.log(mapToUpperCase);
+let foods = ['Bread', 'Milk', 'Tomatos']
+function toUpperCaseMap(foods) {
+    const mapToUpperCase = foods.map( arr => {
+        return arr.toUpperCase();
+    });
+    return mapToUpperCase;
+}
+
+console.log(toUpperCaseMap(foods));
 
 
 
