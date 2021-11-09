c71c99848e0f147c5ddf9a90b0c8537601aa8b33
Maks <maksim.odinbox@gmail.com>
2021-11-09 09:04:11 +0200

    modified

--- homework19/app.js
+++ homework19/app.js
@@ -8,39 +8,55 @@
 
 
 const input = document.getElementById('input-field');
-
 const buttonPlus = document.getElementById('plus');
-
 const buttonMinus = document.getElementById('minus');
-
 const olList = document.getElementById('list');
-
-
-
 const arr = [];
+let activeIndex;
 
 buttonPlus.addEventListener('click', function () {
+    if (!input.value) return;
+    
     arr.push(input.value);
+    
     input.value = ' ';
-    arraySum(arr);
+    
+    renderList(arr);
 });
 
 
 buttonMinus.addEventListener('click', function () {
-    arr.pop(input.value);
-    arraySum(arr);
+    arr.splice(activeIndex, 1);
+    
+    activeIndex = undefined;
+    
+    renderList(arr);
 });
 
-// arr.addEventListener('click', function () {
-//     allLI.style.color = 'green'
-// })
 
-function arraySum(array) {
-    let sum = ' ';
-    for (let i = 0; i < array.length; i++) {
-        sum += `<li>${array[i]}</li>`;
-    }
-    olList.innerHTML = sum;
+olList.addEventListener('click', onClickLi);
+
+function onClickLi(event) {
+    const child = event.target.closest('li');
+    
+    if (!child) return;
+    
+    const index = +child.dataset.index;
+    
+    activeIndex = index;
+    
+    renderList(arr);
 }
-arraySum(arr);
-   
+
+
+function renderList(array) {
+    let htmlString = ' ';
+        
+        for (let i = 0; i < array.length; i++) {
+            htmlString += `<li data-index="${i}" 
+            ${activeIndex === i ? 'class="bg-color"' : ''}> ${array[i]} </li>`;
+        }
+    
+    olList.innerHTML = htmlString;
+}
+renderList(arr);
