27ed87ff3d452b0076e17ca4fa2bda78e834cfd4
Maks <maksim.odinbox@gmail.com>
2021-11-26 18:15:38 +0200

    added homework25

---
+++ homework25/app.js
@@ -0,0 +1,25 @@
+// Расширьте тип данных String методом reverse(), который возвращает строку с обратным порядком символов
+// Модифицируйте метод массива sort таким образом, чтобы он стал немутирующим.
+// Добавьте к коллекции, возвращаемой методом querySelectorAll методы массива map, reduce и find
+
+
+// 1.
+String.prototype.reverse = function () {
+    return this.split(",").reverse().join(" ");
+}
+console.log('1,2,3,4,5'.reverse());
+
+// 2
+Array.prototype.notMutableSorting = Array.prototype.sort;
+
+Array.prototype.sort = function () {
+    return [...this].notMutableSorting();
+}
+console.log(notMutableSorting(1,2,3,4,5));
+
+
+// 3
+NodeList.prototype.map = Array.prototype.map;
+NodeList.prototype.reduce = Array.prototype.reduce;
+NodeList.prototype.find = Array.prototype.find;
+
