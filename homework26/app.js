41d24d105f5a05cedffc5a65d391c8e830d83ca7
Maks <maksim.odinbox@gmail.com>
2021-11-30 13:49:36 +0200

    modified homework26

--- homework26/app.js
+++ homework26/app.js
@@ -98,51 +98,5 @@ class Human extends Animal {
 	}
 }
 
-const human = new Human(2, 'Ivan', 'Petrov');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Animal {
-//     constructor(options) {
-//         this.name = options.name;
-//         this.color = options.color;
-//     }
-
-//     voice() {
-//         console.log('Base voice from:', this.name);
-//     }
-// }
-
-// const dog = new Animal ({name: 'Rex', color: 'white'});
-
-// class Cat extends Animal {
-//     constructor(options) {
-//         super(options);
-
-//         this.hasTail = options.hasTail;
-//         this.type = 'cat';
-//     }
-
-//     voice() {
-//         super.voice()
-//         console.log(this.name + ' says miay')
-//     }
-// }
-
-// const cat = new Cat({name: 'Murzik', color: '#000', hasTail: true})
+const humanIvan = new Human(2, 'Ivan', 'Petrov');
+const humanStepan = new Human(2, 'Stepan', 'Ivanov');
